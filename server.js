e2438544856a1e67ee3ef74f14ac9ed81c219162
Pisiak <scooby_wojtek@o2.pl>
2018-04-08 18:19:08 +0200

    use in building has been removed

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use((req, res, next) => {
    next();
 })
 
-app.use((req, res) => {
-    res.render('bulding.hbs');
-})
+// app.use((req, res) => {
+//     res.render('bulding.hbs');
+// })
 
 
 app.use(express.static(__dirname + '/public'));
